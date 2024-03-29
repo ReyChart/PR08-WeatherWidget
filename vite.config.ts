d7231730e62e618bbc67a670faa5483195a526e0
ReyChart <reycharton@gmail.com>
2024-03-29 16:45:59 +0300

    changed the vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/PR08-WeatherWidget/',
   plugins: [react()],
 
   resolve: {
